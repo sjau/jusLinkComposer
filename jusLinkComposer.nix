9dc7c08077513f586d659e5946cdd50c27f842fe
sjau <github.com@sjau.ch>
2017-08-29 16:06:36 +0200

    updated rev

--- jusLinkComposer.nix
+++ jusLinkComposer.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
 # Switch between local testing and using proper git repo
   src = fetchgit {
     url = https://github.com/sjau/jusLinkComposer.git;
-    rev = "edcecfe407df6722a32d245a10163a3a49869f7c";
+    rev = "5a3155800cebaaef465a4214107a04100069e7a0";
     sha256 = "1iwfyqwzdkj55b517bajjh9yc2ym0gm8zkllj080sxxpzrnwlzxn";
   };
 #  src = /home/hyper/Desktop/git-repos/jusLinkComposer;
